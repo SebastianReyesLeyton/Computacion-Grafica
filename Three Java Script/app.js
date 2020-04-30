0f06f06dffa15b2aece86a0d75eea9f848b92555
Juan <juaninreyes2002@hotmail.com>
2020-04-30 13:45:58 -0500

    Primer acercamiento a Three JS

--- Three Java Script/app.js
+++ Three Java Script/app.js
@@ -1,40 +1,15 @@
-const texture = new THREE.TextureLoader().load("assets/img/texturabalon.jpg")
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(-20, 20, 20)
+'use strict'
 
-var scene = new THREE.Scene()
-var aspect = window.innerWidth / window.innerHeight
-var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000)
-var renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const http = require('http').createServer(webServer),
+    fs = require('fs'),
+    index = fs.createReadStream('./views/index.html')
 
-scene.add(light)
+function webServer(req, res) {
 
-var geometry = new THREE.BoxGeometry(1, 1, 1)
-var material = new THREE.MeshNormalMaterial()
-var cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-camera.position.z = 5
-controls = new THREE.OrbitControls(camera, renderer.domElement)
-
-var loader = new THREE.ObjectLoader()
-
-loader.load("assets/json/bola1.json", function(group) {
-    mesh = group.children[0]
-    mesh.material = new THREE.MeshPhongMaterial({
-        map: texture
-    })
-    mesh.position.x = 5
-    scene.add(mesh)
-})
-
-
-var render = function() {
-    requestAnimationFrame(render)
-    cube.rotation.x += 0.1
-    cube.rotation.y += 0.1
-    renderer.render(scene, camera)
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    index.pipe(res)
 }
 
-render()
+http.listen(3000, 'localhost')
+
+console.log('Server running at http://localhost:3000/')
